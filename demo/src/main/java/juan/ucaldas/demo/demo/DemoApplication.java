55b22e243d4b81369ebb754953973bf4c7a5ed2a
juanma <juan.ramirez4510@ucaldas.edu.co>
2023-09-27 16:28:04 -0500

    cambio mensaje

--- demo/src/main/java/juan/ucaldas/demo/demo/DemoApplication.java
+++ demo/src/main/java/juan/ucaldas/demo/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "mudo") String name) {
 		return String.format("Hello %s!", name);
 	}
 }
